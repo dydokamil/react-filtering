7e0140ce66a592b4657da12f52cf6279ad85389f
kamildydo <dydokamil@gmail.com>
2018-03-07 13:49:43 +0100

    Added eslint to devDependencies; removed logger

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './reducers';
 
-const store = createStore(reducer, applyMiddleware(logger));
+const store = createStore(reducer, applyMiddleware());
 
 ReactDOM.render(
   <Provider store={store}>
